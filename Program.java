6a95f40fc4ee0f75252828576f0cf81613c704e6
MadisonCarr <90936149+MadsionCarr@users.noreply.github.com>
2023-02-09 07:52:55 +0000

    change

---
+++ Program.java
@@ -0,0 +1,20 @@
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        //your code goes here
+        
+        Scanner sc = new Scanner(System.in);
+
+        int celcius = sc.nextInt();
+
+        if(celcius >= 100) {
+System.out.println("Boiling");
+        }
+        else {
+System.out.println("Not Boiling");
+        }
+
+        
+    }
+}
